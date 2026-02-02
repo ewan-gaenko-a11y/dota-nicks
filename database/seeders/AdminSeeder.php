33a254713c6dc07b85457e0e6786a11f5357150b
Ivan Gaenko <ewan.gaenko@yandex.ru>
2026-02-02 21:59:46 +0300

    add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminMail = env('ADMIN_EMAIL');
+        $adminPassword = env('ADMIN_PASSWORD');
+        $adminName = env('ADMIN_NAME', 'Admin');
+
+        $admin = new User();
+        $admin->email = $adminMail;
+        $admin->password = $adminPassword;
+        $admin->name = $adminName;
+        $admin->save();
+    }
+}
